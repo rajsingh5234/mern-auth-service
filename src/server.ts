11501d47ff5d71468b041d5620835147a2f8dcda
Raj Singh <raj02mar2001@gmail.com>
2025-01-05 12:29:02 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from './app'
+import { Config } from './config'
+import logger from './config/logger'
+
+const startServer = () => {
+  const PORT = Config.PORT
+  try {
+    app.listen(PORT, () => {
+      logger.info('Server listening on port', { port: PORT })
+    })
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+startServer()
