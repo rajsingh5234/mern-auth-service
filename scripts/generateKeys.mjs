bdd186879ae61bec5b2e368cf8aba56d70f68aa8
Raj Singh <raj02mar2001@gmail.com>
2025-01-10 22:25:37 +0530

    add key generation script

--- scripts/generateKeys.mjs
+++ scripts/generateKeys.mjs
@@ -1,22 +1,22 @@
-import crypto from "crypto";
-import fs from "fs";
+import crypto from 'crypto'
+import fs from 'fs'
 
-const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-    modulusLength: 2048,
-    publicKeyEncoding: {
-        type: "pkcs1",
-        format: "pem",
-    },
-    privateKeyEncoding: {
-        type: "pkcs1",
-        format: "pem",
-    },
-});
+const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+  publicKeyEncoding: {
+    type: 'pkcs1',
+    format: 'pem',
+  },
+  privateKeyEncoding: {
+    type: 'pkcs1',
+    format: 'pem',
+  },
+})
 
 // eslint-disable-next-line no-undef
-console.log("Public Key", publicKey);
+console.log('Public Key', publicKey)
 // eslint-disable-next-line no-undef
-console.log("Private key", privateKey);
+console.log('Private key', privateKey)
 
-fs.writeFileSync("certs/private.pem", privateKey);
-fs.writeFileSync("certs/public.pem", publicKey);
+fs.writeFileSync('certs/private.pem', privateKey)
+fs.writeFileSync('certs/public.pem', publicKey)
