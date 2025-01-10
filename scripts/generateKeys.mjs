7c4c349ead40bec9110392eaf44735f28d2c7344
Raj Singh <raj02mar2001@gmail.com>
2025-01-10 22:24:47 +0530

    test token generation

---
+++ scripts/generateKeys.mjs
@@ -0,0 +1,22 @@
+import crypto from "crypto";
+import fs from "fs";
+
+const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+    publicKeyEncoding: {
+        type: "pkcs1",
+        format: "pem",
+    },
+    privateKeyEncoding: {
+        type: "pkcs1",
+        format: "pem",
+    },
+});
+
+// eslint-disable-next-line no-undef
+console.log("Public Key", publicKey);
+// eslint-disable-next-line no-undef
+console.log("Private key", privateKey);
+
+fs.writeFileSync("certs/private.pem", privateKey);
+fs.writeFileSync("certs/public.pem", publicKey);
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -4,6 +4,7 @@ import { User } from '../../src/entity/User'
 import { DataSource } from 'typeorm'
 import { AppDataSource } from '../../src/config/data-source'
 import { ROLES } from '../../src/constants'
+import { isJwt } from '../utils'
 
 describe('POST /auth/register', () => {
   let connection: DataSource
@@ -168,6 +169,52 @@ describe('POST /auth/register', () => {
       expect(response.statusCode).toBe(400)
       const users = await userRespository.find()
       expect(users).toHaveLength(1)
+    })
+
+    it('should return the access token and refresh token inside the cookie', async () => {
+      // Arrange
+      const userData = {
+        firstName: 'raj',
+        lastName: 'singh',
+        email: 'raj@gmail.com',
+        password: 'password',
+      }
+
+      // Act
+      const response = await request(app as any)
+        .post('/auth/register')
+        .send(userData)
+
+      // Assert
+
+      // accessToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjkzOTA5Mjc2LCJleHAiOjE2OTM5MDkzMzYsImlzcyI6Im1lcm5zcGFjZSJ9.KetQMEzY36vxhO6WKwSR-P_feRU1yI-nJtp6RhCEZQTPlQlmVsNTP7mO-qfCdBr0gszxHi9Jd1mqf-hGhfiK8BRA_Zy2CH9xpPTBud_luqLMvfPiz3gYR24jPjDxfZJscdhE_AIL6Uv2fxCKvLba17X0WbefJSy4rtx3ZyLkbnnbelIqu5J5_7lz4aIkHjt-rb_sBaoQ0l8wE5KzyDNy7mGUf7cI_yR8D8VlO7x9llbhvCHF8ts6YSBRBt_e2Mjg5txtfBaDq5auCTXQ2lmnJtMb75t1nAFu8KwQPrDYmwtGZDkHUcpQhlP7R-y3H99YnrWpXbP8Zr_oO67hWnoCSw; Max-Age=43200; Domain=localhost; Path=/; Expires=Tue, 05 Sep 2023 22:21:16 GMT; HttpOnly; SameSite=Strict
+      interface Headers {
+        ['set-cookie']: string[]
+      }
+
+      let accessToken = null
+      let refreshToken = null
+
+      const cookies =
+        (response.headers as unknown as Headers)['set-cookie'] || []
+
+      cookies.forEach((cookie) => {
+        if (cookie.startsWith('accessToken=')) {
+          accessToken = cookie.split(';')[0].split('=')[1]
+        }
+
+        if (cookie.startsWith('refreshToken=')) {
+          refreshToken = cookie.split(';')[0].split('=')[1]
+        }
+      })
+
+      expect(accessToken).not.toBeNull()
+      expect(refreshToken).not.toBeNull()
+
+      console.log(accessToken)
+
+      expect(isJwt(accessToken)).toBeTruthy()
+      expect(isJwt(refreshToken)).toBeTruthy()
     })
   })
 
