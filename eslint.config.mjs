c717043a39ab0ca46eaa91a3366f19ab65a423e5
Raj Singh <raj02mar2001@gmail.com>
2025-01-14 12:26:01 +0530

    fix ts build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,16 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   {
-    ignores: ['dist', 'node_modules', 'eslint.config.mjs', 'jest.config.js'],
+    ignores: [
+      'dist',
+      'node_modules',
+      'eslint.config.mjs',
+      'jest.config.js',
+      '*.spec.ts',
+      'tests/',
+      '.github',
+      '*.js',
+    ],
   },
   {
     languageOptions: {
--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -12,7 +12,7 @@ export const AppDataSource = new DataSource({
   // don't use this in production. Always set synchronize = false
   synchronize: false,
   logging: false,
-  entities: ['src/entity/*.ts'],
-  migrations: ['src/migration/*.ts'],
+  entities: ['src/entity/*.{ts,js}'],
+  migrations: ['src/migration/*.{ts,js}'],
   subscribers: [],
 })
