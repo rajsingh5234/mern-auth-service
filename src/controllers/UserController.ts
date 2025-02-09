564ecc9edb2120003a92aecce64a348803732bdc
Raj Singh <raj02mar2001@gmail.com>
2025-02-09 16:36:44 +0530

    improve error handle middleware

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -14,9 +14,7 @@ export class UserController {
     // Validation
     const result = validationResult(req)
     if (!result.isEmpty()) {
-      console.log(result.array())
-      res.status(400).json({ errors: result.array() })
-      return
+      return next(createHttpError(400, result.array()[0].msg as string))
     }
 
     const { firstName, lastName, email, password, tenantId, role } = req.body
